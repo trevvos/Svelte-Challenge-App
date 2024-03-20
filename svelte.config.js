d962ee802dd458ab397d459ae308bad6452b3af0
Lucas Amaral <analista.sistemas.lucas@gmail.com>
2024-03-20 10:52:43 -0300

    adapter static comentado

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,5 @@
 import adapter from '@sveltejs/adapter-auto';
+//import staticAdapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -12,6 +13,7 @@ const config = {
 		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
 		adapter: adapter()
+		//adapter: staticAdapter()
 	}
 };
 
